8ed4dfe938ab8b227b25f9231a6380f4021bedbc
Vaibhav Mishra <48257398+VaibhavMishra173@users.noreply.github.com>
2024-09-23 19:26:31 +0530

    Update program.py, added doc comment

--- 3.Bit Magic/program.py
+++ 3.Bit Magic/program.py
@@ -1,15 +1,45 @@
 def leftShift(n, times):
-  return n<<times
-  
-print(leftShift(3,1))
+    """
+    Performs a left bitwise shift operation.
+    
+    Args:
+        n (int): The number to be shifted.
+        times (int): The number of positions to shift left.
+    
+    Returns:
+        int: The result of shifting 'n' to the left by 'times' positions.
+    """
+    return n << times
+
+print(leftShift(3, 1))  # Output: 6
+
 
 def rightShift(n, times):
-  return n>>times
+    """
+    Performs a right bitwise shift operation.
+    
+    Args:
+        n (int): The number to be shifted.
+        times (int): The number of positions to shift right.
+    
+    Returns:
+        int: The result of shifting 'n' to the right by 'times' positions.
+    """
+    return n >> times
 
-print(rightShift(3,1))
+print(rightShift(3, 1))  # Output: 1
 
 
 def bitwiseNot(n):
-  return ~n
+    """
+    Performs a bitwise NOT operation.
+    
+    Args:
+        n (int): The number to apply the NOT operation on.
+    
+    Returns:
+        int: The result of applying the bitwise NOT on 'n'.
+    """
+    return ~n
 
-print(bitwiseNot(3))
+print(bitwiseNot(3))  # Output: -4
