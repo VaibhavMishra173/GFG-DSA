4fc7b0ed5c4ace9d39fa7d803a98b20d763400a1
Vaibhav Mishra <48257398+VaibhavMishra173@users.noreply.github.com>
2024-10-07 21:17:38 +0530

    Update program.py with insertion in array

--- 5.Arrays/program.py
+++ 5.Arrays/program.py
@@ -16,3 +16,22 @@ def searchArr(l, e):
 
 l = [1, 2, 3, 4, 5]
 print(searchArr(l, 5))
+
+def insertArr(l, p, e):
+    """
+    Inserts an element at a specific position in a list.
+
+    Args:
+        l (list): The list to insert the element into.
+        p (int): The position at which to insert the element.
+        e (any): The element to be inserted.
+
+    Returns:
+        list: A new list with the element inserted at the specified position.
+    """
+    return l[:p] + [e] + l[p:]
+
+l = [1, 2, 3, 4, 5]
+p = 2
+e = 5
+print(insertArr(l, p, e))
